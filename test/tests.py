3813c2044a12246e92ec934dad43431d4ac125fb
Ola <t3nvvk57m@mozmail.com>
2023-07-20 13:44:01 +0200

    Added a test for feature_collection_to_geojson() method.

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,4 @@
-from repository.generate import generate_journey_from_start_stop_coordinates, csv_start_stop_to_journeys, journeys_to_feature_collection
+from repository.generate import generate_journey_from_start_stop_coordinates, csv_start_stop_to_journeys, journeys_to_feature_collection, feature_collection_to_geojson
 from data.model.point import Point
 
 def test_generate_journey_from_start_stop_coordinates():          
@@ -19,13 +19,25 @@ def test_journeys_to_feature_collection():
 
     print("Printing feature collection: ", feature_collection)
 
+def test_feature_collection_to_geojson():
+    filepath_to_csv = "./test/test_file.csv"
+
+    journeys = csv_start_stop_to_journeys(filepath_to_csv)
+
+    feature_collection = journeys_to_feature_collection(journeys)
+
+    feature_collection_to_geojson(feature_collection, "./test/test.geojson")
+
 
 def run_all_tests():
     test_generate_journey_from_start_stop_coordinates()
     test_csv_start_stop_to_journeys()
+    test_journeys_to_feature_collection()
+    test_feature_collection_to_geojson()
 
 # run_all_tests()
 
+# test_generate_journey_from_start_stop_coordinates
 # test_csv_start_stop_to_journeys()
-
-test_journeys_to_feature_collection()
+# test_journeys_to_feature_collection
+# test_feature_collection_to_geojson()
